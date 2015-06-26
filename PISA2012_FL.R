ce900b8974b317753feb742afa509e41c0331310
unknown <esedmik@worldbank.org>
2015-06-26 18:38:18 -0400

    tst

---
+++ PISA2012_FL.R
@@ -0,0 +1,41 @@
+# PISA2012_FL
+# Unraveling a secret: Vietnam's outstanding performance on the PISA test 2012 following the Fryer-Levitt (2004) approach
+
+# Prepared by Elisabeth Sedmik on Wednesday, June 24 2015
+# Based on code by Suhas D. Parandekar
+
+# Revised on ...
+
+# The following code tries to unravel the secret of Vietnam's outstanding performance on the PISA 2012 assessment. 
+# It presents an analytical comparison of possible explanatory factors (as assessed within the PISA 2012 test) 
+# of Vietnam's high test score in MATH, comparing 7 other developing countries with Vietnam. The statistical 
+# approach taken is a modified dummy variable approach following Fryer and Levitt (2004).
+
+# Outline:
+# 1. Generating data set (merging and cleaning) for Vietnam + 7 developing countries
+# 2. Producing descriptive statistics
+# 3. Regression analysis of a modified Fryer-Levitt approach
+
+# Loading R packages to process PISA data:
+
+# Modeling packages
+library(intsvy)# For PISA (and TIMSS, PIRLS, etc) analysis with Plausible Values (PV) and Balanced Repeated Replication (BRR)
+library(TDMR)# For tuned data mining in R - eg. detect column of constants in dataframe
+library(gmodels)# For model fitting, contains various R programming tools (eg. PROC FREQ like tables)
+library(dplyr)# For varioys data manipulation
+library(psych)# For rescaling variables to given mean and sd
+library(sm) # for locally smoothed regressions and density estimation
+library(lme4) # To run mixed-effects models using Eigen and S4
+
+# Admin packages
+library(foreign)# To import and export data to and from R (eg. txt files)
+library(xlsx)# To generate MS-Excel output
+library(xtable)# To generate Latex output (in which the research paper is written)
+library(epicalc)# For producing descriptives of data
+library(tables) # Computes and displays complex tables of summary statistics
+library(stargazer) # For latex regression and summary statistics tables
+
+# Please be aware that many packages (eg. intsvy) require additional packages
+
+
+
