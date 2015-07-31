f41f31d58aa1ea9ce30a1cf0a3622c00d2703d80
unknown <esedmik@worldbank.org>
2015-07-31 16:35:00 -0400

    Vietnam PISA part 4 - (rotated part 1)

--- PISA_2012_FL_part4.R
+++ PISA_2012_FL_part4.R
@@ -114,16 +114,29 @@ N0
 #-----Effort: MATWKETH
 #-----Attitude: INSTMOT, INTMAT, SUBNORM, MATHEFF, FAILMAT, MATINTFC, MATBEH, PERSEV, OPENPS 
 
+# Here we are just double-checking we did not miss any variables, so we just look at the NA's of the rotated 
+# parts and see if they are the same as in 4.2.5 Non-rotated Questions - all gap decreasing/Sensitivity 
+# (File: PISA2012_FL_part2)
+
+T1nonrot <- DEVCON8a[, c("VIETNAM","PROPCERT","SMRATIO","TCSHORT","TCFOCST","SC30Q01","SC30Q02","SC31Q01",
+                         "SC31Q02","SC31Q03","SC31Q04","SC31Q05","SC31Q06","SC31Q07","ST05Q01","REPEAT",
+                         "ST08Q01","ST115Q01","ST28Q01","SC24Q01","PCGIRLS","SC18Q01","SC18Q02","SC18Q05",
+                         "SC39Q07","SC40Q01","SC40Q02","SC03Q01","CLSIZE","COMPWEB","SCMATEDU","SCMATBUI",
+                         "SC16Q02","SC16Q06","SC16Q10","SC16Q11","SC20Q01","SC19Q01","SC39Q03","SCHSEL")]
+Nnonrot <- NROW(na.omit(T1nonrot)) 
+Nnonrot #25612
+N0-Nnonrot #22871 NA's
+
 T1b <- DEVCON8a[, c("VIETNAM","PROPCERT","SMRATIO","TCSHORT","TCFOCST","SC30Q01","SC30Q02","SC31Q01",
                     "SC31Q02","SC31Q03","SC31Q04","SC31Q05","SC31Q06","SC31Q07","ST05Q01","REPEAT",
                     "ST08Q01","ST115Q01","ST28Q01","SC24Q01","PCGIRLS","SC18Q01","SC18Q02","SC18Q05",
                     "SC39Q07","SC40Q01","SC40Q02","SC03Q01","CLSIZE","COMPWEB","SCMATEDU","SCMATBUI",
-                    "SC16Q02","SC16Q06","SC16Q10","SC16Q11","SC20Q01","SC19Q01","LEADINST","SC39Q03",
-                    "SCHSEL", "MATWKETH","PERSEV","OPENPS","INTMAT","INSTMOT","SUBNORM","MATHEFF","FAILMAT",
+                    "SC16Q02","SC16Q06","SC16Q10","SC16Q11","SC20Q01","SC19Q01","SC39Q03","SCHSEL",
+                    "MATWKETH","PERSEV","OPENPS","INTMAT","INSTMOT","SUBNORM","MATHEFF","FAILMAT",
                     "MATINTFC","MATBEH")]
 N1 <- NROW(na.omit(T1b)) 
-N1 #15616
-N0-N1 #32867 NA's
+N1 #15660
+N0-N1 #32823 NA's
 DEVCON8i <- DEVCON8a[complete.cases(T1b),]
 
 # Let's prepare the relevant student variables again:
@@ -209,7 +222,7 @@ DEVCON8i$DUM_SMLTOWN <- ifelse(DEVCON8i$SC03Q01==2,1,0)
 DEVCON8i$DUM_TOWN    <- ifelse(DEVCON8i$SC03Q01==3,1,0)
 
 DEVCON8i$TOWN <- DEVCON8i$DUM_SMLTOWN+DEVCON8i$DUM_TOWN
-DEVCON8i$TOWN[DEVCON8i$TOWN>1] <- 1
+# DEVCON8i$TOWN[DEVCON8i$TOWN>1] <- 1 do not need to do this since mutually exclusive
 
 #SC16Q01-Q11
 #________________________________________________________________________________________________________
@@ -253,9 +266,9 @@ R103 <- pisa.reg.pv(pvlabel="MATH",
                    data=DEVCON8i,export=FALSE)
 R103
 #Estimate Std. Error t value
-#(Intercept)   396.09       2.92  135.43
-#VIETNAM       119.94       6.77   17.72
-#R-squared      27.27       2.54   10.75
+#(Intercept)   396.11       2.92  135.69
+#VIETNAM       119.92       6.76   17.73
+#R-squared      27.26       2.53   10.76
 
 # Let's try our regression with all gap decreasing variables before we add the rotated parts
 
@@ -271,46 +284,46 @@ R104 <- pisa.reg.pv(pvlabel="MATH",
                    data=DEVCON8i,export=FALSE)
 R104
 #Estimate Std. Error t value
-#(Intercept)     354.09      20.94   16.91
-#VIETNAM          73.32       7.76    9.45
+#(Intercept)     354.12      20.95   16.90
+#VIETNAM          73.29       7.75    9.45
 #PRESCHOOL        27.40       3.73    7.35
-#REPEAT          -34.74       3.85   -9.02
-#ST08Q01          -7.35       1.57   -4.69
-#ST115Q01         -4.91       2.03   -2.42
-#BOOK_N            0.07       0.01    5.05
-#PARPRESSURE      10.20       4.29    2.38
-#PCGIRLS           8.33      14.22    0.59
+#REPEAT          -34.65       3.86   -8.99
+#ST08Q01          -7.33       1.57   -4.68
+#ST115Q01         -4.88       2.03   -2.40
+#BOOK_N            0.07       0.01    5.06
+#PARPRESSURE      10.24       4.29    2.39
+#PCGIRLS           8.36      14.21    0.59
 #FUNDMOM           0.17       0.07    2.58
-#COUNCILMOM       -0.14       0.06   -2.30
-#PROPCERT         17.08       6.75    2.53
+#COUNCILMOM       -0.14       0.06   -2.31
+#PROPCERT         17.05       6.76    2.52
 #SMRATIO          -0.03       0.01   -2.23
-#TCSHORT           3.58       1.79    2.00
-#TCFOCST          -1.43       1.93   -0.74
-#TCM_STUASS       -1.46       7.72   -0.19
-#TCM_PEER         -5.46       5.65   -0.97
-#TCH_INCENTV      -3.13       2.58   -1.21
-#ASS_PROG        -24.68       7.69   -3.21
-#ASS_PROM         13.79       5.86    2.35
-#ASS_SCH          -2.48       7.94   -0.31
-#STU_FEEDB         2.33       5.01    0.47
-#COMP_USE         -0.40       5.38   -0.07
-#TXT_BOOK        -10.51       7.03   -1.49
-#TOWN             -8.26       3.42   -2.41
-#CLSIZE            0.77       0.23    3.34
-#COMPWEB          11.32       6.25    1.81
-#SCMATEDU          5.86       3.06    1.92
-#SCMATBUI          4.40       2.50    1.76
-#EXC2_PLAY         8.22       3.97    2.07
-#EXC6_MATHCOMP     0.39       5.32    0.07
-#EXC10_SPORT      -4.21       9.91   -0.43
-#EXC11_UNICORN     6.90       5.56    1.24
-#SCL_EXTR_CL      11.32       5.22    2.17
-#SCORE_PUBLIC     10.26       4.86    2.11
-#QUAL_RECORD       8.49       6.40    1.33
-#SCHSEL            0.63       3.10    0.20
-#R-squared        43.03       2.33   18.49
+#TCSHORT           3.56       1.79    1.99
+#TCFOCST          -1.46       1.93   -0.75
+#TCM_STUASS       -1.35       7.73   -0.17
+#TCM_PEER         -5.54       5.64   -0.98
+#TCH_INCENTV      -3.11       2.58   -1.21
+#ASS_PROG        -24.56       7.71   -3.19
+#ASS_PROM         13.83       5.86    2.36
+#ASS_SCH          -2.35       7.95   -0.30
+#STU_FEEDB         2.29       5.01    0.46
+#COMP_USE         -0.39       5.37   -0.07
+#TXT_BOOK        -10.47       7.04   -1.49
+#TOWN             -8.24       3.42   -2.41
+#CLSIZE            0.76       0.23    3.32
+#COMPWEB          11.29       6.25    1.81
+#SCMATEDU          5.87       3.06    1.92
+#SCMATBUI          4.39       2.50    1.76
+#EXC2_PLAY         8.23       3.97    2.07
+#EXC6_MATHCOMP     0.36       5.32    0.07
+#EXC10_SPORT      -4.27       9.87   -0.43
+#EXC11_UNICORN     6.90       5.55    1.24
+#SCL_EXTR_CL      11.37       5.21    2.18
+#SCORE_PUBLIC     10.22       4.86    2.10
+#QUAL_RECORD       8.25       6.43    1.28
+#SCHSEL            0.66       3.10    0.21
+#R-squared        43.02       2.33   18.50
 
-# So with all the gap decreasing non rotated variables, the Vietnam dummy goes down to 73.32
+# So with all the gap decreasing non rotated variables, the Vietnam dummy goes down to 73.29
 
 R105 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -323,7 +336,7 @@ R105 <- pisa.reg.pv(pvlabel="MATH",
                     weight="W_FSTUWT",
                     data=DEVCON8i,export=FALSE)
 R105 # MATWKETH decreases
-#VIETNAM: 73.05
+#VIETNAM: 73.02
 
 R106 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -336,7 +349,7 @@ R106 <- pisa.reg.pv(pvlabel="MATH",
                     weight="W_FSTUWT",
                     data=DEVCON8i,export=FALSE)
 R106 # MATWKETH decreases, INSTMOT decreases
-#VIETNAM: 71.87
+#VIETNAM: 71.83
 
 R107 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -349,7 +362,7 @@ R107 <- pisa.reg.pv(pvlabel="MATH",
                     weight="W_FSTUWT",
                     data=DEVCON8i,export=FALSE)
 R107 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases
-#VIETNAM: 71.26
+#VIETNAM: 71.23
 
 R108 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -363,7 +376,7 @@ R108 <- pisa.reg.pv(pvlabel="MATH",
                     weight="W_FSTUWT",
                     data=DEVCON8i,export=FALSE)
 R108 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases, SUBNORM decreases (drastically!)
-#VIETNAM: 64.09
+#VIETNAM: 64.06
 
 R109 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -377,7 +390,7 @@ R109 <- pisa.reg.pv(pvlabel="MATH",
                     weight="W_FSTUWT",
                     data=DEVCON8i,export=FALSE)
 R109 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases, SUBNORM decreases (drastically!), MATHEF decreases
-#VIETNAM: 61.70
+#VIETNAM: 61.67
 
 R110 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -392,7 +405,7 @@ R110 <- pisa.reg.pv(pvlabel="MATH",
                     data=DEVCON8i,export=FALSE)
 R110 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases, SUBNORM decreases (drastically!), MATHEF decreases
 # FAILMAT increases
-#VIETNAM: 63.22
+#VIETNAM: 63.19
 
 R111 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -407,7 +420,7 @@ R111 <- pisa.reg.pv(pvlabel="MATH",
                     data=DEVCON8i,export=FALSE)
 R111 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases, SUBNORM decreases (drastically!), MATHEF decreases
 # FAILMAT increases, MATINTFC decreases
-#VIETNAM: 61.86
+#VIETNAM: 61.83
 
 R112 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -422,7 +435,7 @@ R112 <- pisa.reg.pv(pvlabel="MATH",
                     data=DEVCON8i,export=FALSE)
 R112 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases, SUBNORM decreases (drastically!), MATHEF decreases
 # FAILMAT increases, MATINTFC decreases, MATBEH increases
-#VIETNAM: 62.17
+#VIETNAM: 62.14
 
 R113 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -437,7 +450,7 @@ R113 <- pisa.reg.pv(pvlabel="MATH",
                     data=DEVCON8i,export=FALSE)
 R113 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases, SUBNORM decreases (drastically!), MATHEF decreases
 # FAILMAT increases, MATINTFC decreases, MATBEH increases, PERSEV decreases
-#VIETNAM: 61.57
+#VIETNAM: 61.54
 
 R114 <- pisa.reg.pv(pvlabel="MATH", 
                     x=c("VIETNAM",
@@ -452,7 +465,7 @@ R114 <- pisa.reg.pv(pvlabel="MATH",
                     data=DEVCON8i,export=FALSE)
 R114 # MATWKETH decreases, INSTMOT decreases, INTMAT decreases, SUBNORM decreases (drastically!), MATHEF decreases
 # FAILMAT increases, MATINTFC decreases, MATBEH increases, PERSEV decreases, OPENPS increases
-#VIETNAM: 64.40
+#VIETNAM: 64.38
 
 # Let's check all the gap-decreasing variables together:
 
@@ -469,51 +482,51 @@ R115 <- pisa.reg.pv(pvlabel="MATH",
                     data=DEVCON8i,export=FALSE)
 R115
 #Estimate Std. Error t value
-#(Intercept)     360.09      19.38   18.58
-#VIETNAM          59.91       6.92    8.66
+#(Intercept)     360.18      19.37   18.59
+#VIETNAM          59.88       6.91    8.66
 #PRESCHOOL        22.54       3.48    6.48
-#REPEAT          -30.69       3.67   -8.37
-#ST08Q01          -5.88       1.60   -3.68
-#ST115Q01         -4.28       1.90   -2.25
-#BOOK_N            0.05       0.01    4.14
-#PARPRESSURE       9.13       3.97    2.30
-#PCGIRLS           7.59      13.09    0.58
+#REPEAT          -30.60       3.67   -8.34
+#ST08Q01          -5.87       1.59   -3.68
+#ST115Q01         -4.26       1.90   -2.24
+#BOOK_N            0.05       0.01    4.15
+#PARPRESSURE       9.16       3.96    2.31
+#PCGIRLS           7.62      13.08    0.58
 #FUNDMOM           0.16       0.06    2.76
-#COUNCILMOM       -0.14       0.05   -2.64
-#PROPCERT         16.31       6.18    2.64
+#COUNCILMOM       -0.14       0.05   -2.65
+#PROPCERT         16.28       6.19    2.63
 #SMRATIO          -0.02       0.01   -2.22
-#TCSHORT           2.14       1.63    1.31
-#TCFOCST          -0.32       1.72   -0.19
-#TCM_STUASS       -1.38       7.32   -0.19
-#TCM_PEER         -4.89       5.28   -0.93
-#TCH_INCENTV      -2.97       2.24   -1.33
-#ASS_PROG        -16.29       7.75   -2.10
-#ASS_PROM         10.38       5.49    1.89
-#ASS_SCH           1.22       7.61    0.16
-#STU_FEEDB         2.07       4.50    0.46
-#COMP_USE         -0.65       5.04   -0.13
-#TXT_BOOK         -7.22       6.31   -1.14
-#TOWN             -5.99       3.18   -1.88
-#CLSIZE            0.69       0.21    3.26
-#COMPWEB          12.97       5.71    2.27
-#SCMATEDU          4.75       2.73    1.74
-#SCMATBUI          3.17       2.27    1.40
-#EXC2_PLAY         5.87       3.72    1.58
-#EXC6_MATHCOMP    -0.82       4.81   -0.17
-#EXC10_SPORT      -4.65       9.15   -0.51
-#EXC11_UNICORN     7.06       4.82    1.46
-#SCL_EXTR_CL      11.19       4.99    2.24
-#SCORE_PUBLIC     10.74       4.37    2.45
-#QUAL_RECORD      11.97       6.21    1.93
-#SCHSEL            1.64       2.88    0.57
-#MATWKETH         -9.89       1.58   -6.25
-#INSTMOT           5.76       1.29    4.48
-#INTMAT           -3.37       1.88   -1.79
-#SUBNORM         -12.14       0.90  -13.54
-#MATHEFF          29.90       2.05   14.58
-#MATINTFC          8.44       0.85    9.94
-#PERSEV            4.38       1.10    3.96
-#R-squared        49.61       2.08   23.89
+#TCSHORT           2.12       1.63    1.30
+#TCFOCST          -0.35       1.72   -0.20
+#TCM_STUASS       -1.26       7.33   -0.17
+#TCM_PEER         -4.97       5.26   -0.94
+#TCH_INCENTV      -2.95       2.24   -1.32
+#ASS_PROG        -16.17       7.75   -2.09
+#ASS_PROM         10.42       5.48    1.90
+#ASS_SCH           1.35       7.62    0.18
+#STU_FEEDB         2.03       4.50    0.45
+#COMP_USE         -0.64       5.04   -0.13
+#TXT_BOOK         -7.18       6.31   -1.14
+#TOWN             -5.97       3.18   -1.88
+#CLSIZE            0.69       0.21    3.24
+#COMPWEB          12.93       5.71    2.27
+#SCMATEDU          4.75       2.72    1.74
+#SCMATBUI          3.16       2.26    1.40
+#EXC2_PLAY         5.88       3.72    1.58
+#EXC6_MATHCOMP    -0.85       4.81   -0.18
+#EXC10_SPORT      -4.71       9.12   -0.52
+#EXC11_UNICORN     7.04       4.81    1.46
+#SCL_EXTR_CL      11.23       4.98    2.26
+#SCORE_PUBLIC     10.71       4.37    2.45
+#QUAL_RECORD      11.71       6.24    1.88
+#SCHSEL            1.67       2.88    0.58
+#MATWKETH         -9.87       1.58   -6.26
+#INSTMOT           5.76       1.28    4.49
+#INTMAT           -3.39       1.88   -1.81
+#SUBNORM         -12.14       0.90  -13.56
+#MATHEFF          29.90       2.05   14.61
+#MATINTFC          8.43       0.85    9.95
+#PERSEV            4.37       1.10    3.97
+#R-squared        49.60       2.08   23.90
 
 # Let's check all gap increasing variables together (of course with the gap decreasing non-rotated part, as always):
 
@@ -529,46 +542,46 @@ R116 <- pisa.reg.pv(pvlabel="MATH",
                     data=DEVCON8i,export=FALSE)
 R116
 #Estimate Std. Error t value
-#(Intercept)     353.47      21.36   16.55
-#VIETNAM          80.05       7.71   10.38
+#(Intercept)     353.51      21.37   16.54
+#VIETNAM          80.03       7.70   10.39
 #PRESCHOOL        26.53       3.69    7.18
-#REPEAT          -34.06       3.92   -8.68
-#ST08Q01          -6.97       1.57   -4.44
-#ST115Q01         -3.94       2.03   -1.94
-#BOOK_N            0.07       0.01    4.80
-#PARPRESSURE       9.77       4.29    2.28
-#PCGIRLS           7.39      14.55    0.51
+#REPEAT          -33.98       3.93   -8.65
+#ST08Q01          -6.96       1.57   -4.43
+#ST115Q01         -3.91       2.03   -1.93
+#BOOK_N            0.07       0.01    4.81
+#PARPRESSURE       9.80       4.29    2.29
+#PCGIRLS           7.42      14.55    0.51
 #FUNDMOM           0.17       0.07    2.61
-#COUNCILMOM       -0.15       0.06   -2.45
-#PROPCERT         17.96       6.73    2.67
+#COUNCILMOM       -0.15       0.06   -2.47
+#PROPCERT         17.93       6.74    2.66
 #SMRATIO          -0.02       0.01   -1.98
-#TCSHORT           3.71       1.78    2.08
-#TCFOCST          -1.46       1.92   -0.76
-#TCM_STUASS       -1.77       7.75   -0.23
-#TCM_PEER         -4.25       5.61   -0.76
-#TCH_INCENTV      -3.18       2.56   -1.24
-#ASS_PROG        -25.33       7.81   -3.24
-#ASS_PROM         13.98       5.99    2.34
-#ASS_SCH          -2.61       8.19   -0.32
-#STU_FEEDB         2.64       4.97    0.53
-#COMP_USE         -0.30       5.41   -0.06
-#TXT_BOOK         -9.45       7.19   -1.31
-#TOWN             -7.80       3.42   -2.28
-#CLSIZE            0.75       0.23    3.27
-#COMPWEB          11.54       6.21    1.86
-#SCMATEDU          6.16       3.06    2.01
-#SCMATBUI          4.01       2.53    1.58
-#EXC2_PLAY         7.79       3.96    1.97
-#EXC6_MATHCOMP    -0.50       5.33   -0.09
-#EXC10_SPORT      -3.82       9.99   -0.38
-#EXC11_UNICORN     6.61       5.62    1.18
-#SCL_EXTR_CL      11.41       5.22    2.19
-#SCORE_PUBLIC     10.42       4.91    2.12
-#QUAL_RECORD       9.82       6.34    1.55
-#SCHSEL            0.92       3.14    0.29
-#FAILMAT          -7.11       1.13   -6.29
-#MATBEH           -4.20       1.67   -2.52
-#OPENPS            9.38       1.25    7.49
-#R-squared        44.16       2.30   19.20
+#TCSHORT           3.69       1.78    2.07
+#TCFOCST          -1.49       1.91   -0.78
+#TCM_STUASS       -1.66       7.76   -0.21
+#TCM_PEER         -4.32       5.60   -0.77
+#TCH_INCENTV      -3.16       2.56   -1.24
+#ASS_PROG        -25.21       7.83   -3.22
+#ASS_PROM         14.02       5.98    2.35
+#ASS_SCH          -2.49       8.20   -0.30
+#STU_FEEDB         2.60       4.97    0.52
+#COMP_USE         -0.29       5.41   -0.05
+#TXT_BOOK         -9.40       7.19   -1.31
+#TOWN             -7.79       3.43   -2.27
+#CLSIZE            0.75       0.23    3.26
+#COMPWEB          11.51       6.20    1.85
+#SCMATEDU          6.17       3.06    2.02
+#SCMATBUI          4.00       2.53    1.58
+#EXC2_PLAY         7.80       3.96    1.97
+#EXC6_MATHCOMP    -0.53       5.33   -0.10
+#EXC10_SPORT      -3.86       9.95   -0.39
+#EXC11_UNICORN     6.60       5.61    1.18
+#SCL_EXTR_CL      11.46       5.21    2.20
+#SCORE_PUBLIC     10.39       4.90    2.12
+#QUAL_RECORD       9.56       6.37    1.50
+#SCHSEL            0.95       3.14    0.30
+#FAILMAT          -7.12       1.13   -6.30
+#MATBEH           -4.21       1.67   -2.53
+#OPENPS            9.40       1.25    7.50
+#R-squared        44.15       2.30   19.21
 
 
